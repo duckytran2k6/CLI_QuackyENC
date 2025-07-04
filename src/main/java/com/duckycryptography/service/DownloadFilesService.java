263a19be244ec7b93ad1d224145d52971aee4074
Ducky Tran <vu0ng.tr4nduc@gmail.com>
2025-07-04 08:10:57 -0400

    Changed input variable name from zipFile to file

--- src/main/java/com/duckycryptography/service/DownloadFilesService.java
+++ src/main/java/com/duckycryptography/service/DownloadFilesService.java
@@ -9,17 +9,17 @@ import java.nio.file.Path;
 
 public class DownloadFilesService {
 
-    public static void moveToDest(String userDest, File zipFile) throws IOException {
+    public static void moveToDest(String userDest, File file) throws IOException {
         File outputDest = new File(userDest);
 
         if (!outputDest.exists()) {
             outputDest.mkdirs();
         }
 
-        Path destPath = Path.of(outputDest.getAbsolutePath(), zipFile.getName());
-        Files.copy(zipFile.toPath(), destPath);
+        Path destPath = Path.of(outputDest.getAbsolutePath(), file.getName());
+        Files.copy(file.toPath(), destPath);
 
-        cleanUpTempDir(zipFile.getParentFile());
+        cleanUpTempDir(file.getParentFile());
     }
 
     public static void cleanUpTempDir(File tempFolder) throws IOException {
