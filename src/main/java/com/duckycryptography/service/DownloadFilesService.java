4b03518720c21c9e6f1ef9abf123020b9125f891
Ducky Tran <vu0ng.tr4nduc@gmail.com>
2025-07-04 08:09:52 -0400

    Added methods for DownloadService class: 1. moveToDest method to handle the user's desire directory to where they want to save their encrypted zip file or the decrypted file 2. cleanUpTempDir method to clean up the files in the temp folder after the download

--- src/main/java/com/duckycryptography/service/DownloadFilesService.java
+++ src/main/java/com/duckycryptography/service/DownloadFilesService.java
@@ -1,5 +1,30 @@
 package com.duckycryptography.service;
 
+import org.springframework.util.FileSystemUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class DownloadFilesService {
 
+    public static void moveToDest(String userDest, File zipFile) throws IOException {
+        File outputDest = new File(userDest);
+
+        if (!outputDest.exists()) {
+            outputDest.mkdirs();
+        }
+
+        Path destPath = Path.of(outputDest.getAbsolutePath(), zipFile.getName());
+        Files.copy(zipFile.toPath(), destPath);
+
+        cleanUpTempDir(zipFile.getParentFile());
+    }
+
+    public static void cleanUpTempDir(File tempFolder) throws IOException {
+        if (tempFolder.exists()) {
+            FileSystemUtils.deleteRecursively(tempFolder);
+        }
+    }
 }
