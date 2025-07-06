0146dabea65e785b04a12c432375442bb32d53fe
Ducky Tran <vu0ng.tr4nduc@gmail.com>
2025-07-06 11:09:29 -0400

    Marked RootCommand as a Spring component for the auto-wired. Adding some 'Hello World' testing code

--- src/main/java/com/duckycryptography/CLI/RootCommand.java
+++ src/main/java/com/duckycryptography/CLI/RootCommand.java
@@ -1,4 +1,19 @@
 package com.duckycryptography.CLI;
 
-public class RootCommand {
+import org.springframework.stereotype.Component;
+import picocli.CommandLine;
+
+@Component
+@CommandLine.Command(name = "hello", description = "Says Hello")
+public class RootCommand implements Runnable {
+    public static void main(String[] args) {
+         CommandLine.run(new RootCommand(), args);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello World!");
+    }
+
+
 }
