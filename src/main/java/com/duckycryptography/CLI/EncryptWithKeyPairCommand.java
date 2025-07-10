dfbaea0f79c9e931f82ebbcb05998a1ecd4385f9
Ducky Tran <vu0ng.tr4nduc@gmail.com>
2025-07-10 12:13:03 -0400

    Adding Parameter annotation to EKP class

--- src/main/java/com/duckycryptography/CLI/EncryptWithKeyPairCommand.java
+++ src/main/java/com/duckycryptography/CLI/EncryptWithKeyPairCommand.java
@@ -1,6 +1,8 @@
 package com.duckycryptography.CLI;
-
+import com.duckycryptography.service.EncryptService;
 import picocli.CommandLine;
+
+import java.io.File;
 
 @CommandLine.Command (
         name = "-e-kp",
@@ -8,8 +10,27 @@ import picocli.CommandLine;
 )
 
 public class EncryptWithKeyPairCommand implements Runnable {
+
+    @CommandLine.Parameters(arity = "2", description = "Please upload the plain text file and the public key!")
+    private File[] files;
+
     @Override
     public void run() {
-        System.out.println("Encrypting with key pair!");
+        if (files == null || files.length != 2) {
+            System.err.println("Please upload the files as specified above!");
+            return;
+        }
+        File inputFile = files[0];
+        File publicKeyFile = files[1];
+
+        System.out.println("inputFile: " + inputFile.getAbsolutePath());
+        System.out.println("publicKeyFile: " + publicKeyFile.getAbsolutePath());
+
+        EncryptService eKP = new EncryptService();
+        try {
+            eKP.encryptDataWithKeyPair(inputFile, publicKeyFile);
+        } catch (Exception e) {
+           System.err.println(e.getMessage());
+        }
     }
 }
