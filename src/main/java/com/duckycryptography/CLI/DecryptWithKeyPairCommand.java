ad8667d49818352b85a0862f958aa9fec6ef8334
Ducky Tran <vu0ng.tr4nduc@gmail.com>
2025-07-12 12:00:47 -0400

    Adding Parameter annotation to DKP class

--- src/main/java/com/duckycryptography/CLI/DecryptWithKeyPairCommand.java
+++ src/main/java/com/duckycryptography/CLI/DecryptWithKeyPairCommand.java
@@ -1,6 +1,10 @@
 package com.duckycryptography.CLI;
 
+import com.duckycryptography.service.DecryptService;
+import com.duckycryptography.service.EncryptService;
 import picocli.CommandLine;
+
+import java.io.File;
 
 @CommandLine.Command (
         name = "-d-kp",
@@ -8,8 +12,31 @@ import picocli.CommandLine;
 )
 
 public class DecryptWithKeyPairCommand implements Runnable {
+
+
+    @CommandLine.Parameters(arity = "3", description = "Please upload the encrypted file, encrypted key IV, and the private key!")
+    private File[] files;
+
     @Override
     public void run() {
-        System.out.println("Decrypting with key pair!");
+
+        if (files == null || files.length != 3) {
+            System.err.println("Please upload the files as specified above!");
+            return;
+        }
+        File inputFile = files[0];
+        File keyIVFile = files[1];
+        File privateKeyFile = files[2];
+
+        System.out.println("inputFile: " + inputFile.getAbsolutePath());
+        System.out.println("keyIVFile: " + keyIVFile.getAbsolutePath());
+        System.out.println("privateKeyFile: " + privateKeyFile.getAbsolutePath());
+
+        DecryptService dKP = new DecryptService();
+        try {
+            dKP.decryptWithKeyPair(inputFile, keyIVFile, privateKeyFile);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
