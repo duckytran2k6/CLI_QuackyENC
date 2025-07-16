21e1f52eb69c323779dbe2e60ae846eccaf5f8f6
Ducky Tran <vu0ng.tr4nduc@gmail.com>
2025-07-16 07:29:39 -0400

    1. Added generate-version script which automatically run git describe command to get the latest annotated tag 2. Added plugin to ensure that the script runs everytime the code is built (make sure the version stays up to date with Git) 3. Simplified VersionProvider class to only prints out the version.

--- src/main/java/com/duckycryptography/customs/VersionProvider.java
+++ src/main/java/com/duckycryptography/customs/VersionProvider.java
@@ -1,7 +1,6 @@
 package com.duckycryptography.customs;
 
 import picocli.CommandLine;
-
 import java.io.InputStream;
 import java.util.Properties;
 
@@ -9,22 +8,9 @@ public class VersionProvider implements CommandLine.IVersionProvider {
     @Override
     public String[] getVersion() throws Exception {
         Properties props = new Properties();
-
-        try (InputStream stream = getClass().getClassLoader().getResourceAsStream("git.properties")) {
-            if (stream == null) {
-                return new String[]{"DuckyTool version info not available"};
-            }
-
-            props.load(stream);
-            String version = props.getProperty("git.build.version", "unknown");
-            String commit = props.getProperty("git.commit.id.abbrev", "unknown");
-            String branch = props.getProperty("git.branch", "unknown");
-
-            return new String[]{
-                    "DuckyTool v" + version,
-                    "Commit: " + commit,
-                    "Branch: " + branch
-            };
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("version.properties")) {
+            props.load(input);
+            return new String[]{props.getProperty("version", "v0.0.0")};
         }
     }
 }
