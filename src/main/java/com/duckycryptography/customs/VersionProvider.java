785f2d97d9be33c9c436e7cbf0772d4c545b2238
Ducky Tran <vu0ng.tr4nduc@gmail.com>
2025-07-16 07:05:33 -0400

    1. pom.xml: Integrate git-based versioning with maven plugin 2. Auto-generate version info from git 3. Update VersionProvider then call it in RootCommand -v/--version flag => AUTOMATIC VERSION INJECTION FROM GIT METADATA

--- src/main/java/com/duckycryptography/customs/VersionProvider.java
+++ src/main/java/com/duckycryptography/customs/VersionProvider.java
@@ -2,19 +2,29 @@ package com.duckycryptography.customs;
 
 import picocli.CommandLine;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class VersionProvider implements CommandLine.IVersionProvider {
     @Override
-    public String[] getVersion() {
+    public String[] getVersion() throws Exception {
         Properties props = new Properties();
-        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("git.properties")) {
-            props.load(inputStream);
-            return new String[]{"DuckyTool v" + props.getProperty("Version")};
-        } catch (IOException e) {
-            return new String[]{"DuckyTool version info not available"};
+
+        try (InputStream stream = getClass().getClassLoader().getResourceAsStream("git.properties")) {
+            if (stream == null) {
+                return new String[]{"DuckyTool version info not available"};
+            }
+
+            props.load(stream);
+            String version = props.getProperty("git.build.version", "unknown");
+            String commit = props.getProperty("git.commit.id.abbrev", "unknown");
+            String branch = props.getProperty("git.branch", "unknown");
+
+            return new String[]{
+                    "DuckyTool v" + version,
+                    "Commit: " + commit,
+                    "Branch: " + branch
+            };
         }
     }
 }
